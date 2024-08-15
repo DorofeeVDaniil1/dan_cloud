3912d0f34741141b691ca9cce6a41faa44d80160
dandy <d.dorofeev@smartdata.dev>
2024-08-15 18:14:40 +0800

    Первый запуск Spring приложения. Работа с контроллером и создание теста с помощью JUnit.

---
+++ src/main/java/com/example/dan_cloud/DanCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.dan_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dan_cloud/DanCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dan_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
